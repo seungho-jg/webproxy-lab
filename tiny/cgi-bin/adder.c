49964901eae00510927ef409cd7b370eca26c2c1
Ubuntu <ubuntu@ip-172-31-29-78.ec2.internal>
2024-05-07 06:50:36 +0000

    first

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -5,6 +5,26 @@
 #include "csapp.h"
 
 int main(void) {
+  char *buf, *p;
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+  int n1=0, n2=0;
+
+  if ((buf = getenv("QUERY_STRING")) != NULL) {
+    p = strchr(buf, '&');
+    *p = '\0';
+    strcpy(arg1, buf);
+    strcpy(arg2, p+1);
+    n1 = atoi(arg1);
+    n2 = atoi(arg2);
+  }
+  sprintf(content, "QUERY_STRING=%s", buf);
+  sprintf(content, "Welcom to add.com: ");
+  sprintf(content, "%sThe Interet addition portal.\r\n", content);
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+  sprintf(content, "%sThanks for visiting!\r\n", content);
+
+  
+  fflush(stdout);
   exit(0);
 }
 /* $end adder */
